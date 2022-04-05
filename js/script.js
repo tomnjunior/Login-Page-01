6498ae4dae529962ad2115be5d943394dcd6d50c
Tom Nonato Junior <77701041+tomnjunior@users.noreply.github.com>
2022-04-05 16:27:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ const btneye = document.querySelector('.btn-eye');
 btneye.onclick = () =>{
     if (senha.type === 'password') {
         senha.type = 'text'
-        btneye.src = '../img/eye_show.png'
+        btneye.src = './img/eye_show.png'
     } else {
         senha.type = 'password'
-        btneye.src = '../img/eye_hide.png'
+        btneye.src = './img/eye_hide.png'
     }
 }
+
